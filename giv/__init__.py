988fe116c3216aa37b5cd5faa620b4f20ae29e0e
AndresSp <atunon@hypernovalabs.com>
2018-07-12 11:06:07 -0500

    Improve Structure Project and Include CSS

--- giv/__init__.py
+++ giv/__init__.py
@@ -2,3 +2,18 @@ from flask import Flask
 app = Flask(__name__)
 
 import giv.views
+
+def create_app(debug=True):
+    app = Flask(__name__)
+    app.debug = debug
+
+    # add your modules
+    app.register_module(giv.views)
+    
+    # other setup tasks
+
+    return app
+
+if __name__ == "__main__":
+    app = create_app(debug=True)
+    app.run()
--- giv/views.py
+++ giv/views.py
@@ -1,5 +1,12 @@
+import os
+from flask import send_from_directory, render_template
 from giv import app
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
