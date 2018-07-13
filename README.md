9b8f2dc7d7bcf264f6885601fbbb7068c175de20
AndresSp <atunon@hypernovalabs.com>
2018-07-13 05:42:35 -0500

    Set setup and Readme Instructions

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 - Generate image variations and its XML to create Datasets or any other purpose
 - Train a Signal Classifier that help a blind recognize signals
 
+### Requirements
+- Python 2 or 3
+
+## Development
+This repository includes scripts to enable easier development of the application. ,To install all required packages run the following command
+```shell
+python setup.py install
+```
+
+To start the whole application ecosystem for development, start the virtual environment and local server
+```shell
+. venv/bin/activate
+export FLASK_APP=seeThatSign
+export FLASK_ENV=development
+python3 -m flask run
+```
+
--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from setuptools import setup
 
+import os
+
+os.environ['FLASK_APP'] = 'seeThatSign'
+
 setup(
     name='seeThatSign',
+    version='1.0',
+    description='Generate Datasets and Train a Signal Classifier',
+    long_description='Generate image variations and its XML to create Datasets or any other purpose and Train a Signal Classifier that help a blind recognize signals',
+    author='AndresSp, c4explosive, Martin Coronado',
+    author_email='andrestunonsp@gmail.com, ao946391@gmail.com, undefined',
+    url='https://github.com/AndresSp/SeeThatSign',
+    license='MIT',
+    platforms=['Browsers'],
     packages=['seeThatSign'],
     include_package_data=True,
     install_requires=[
-        'flask',
+        'flask>=1.0.2',
+        'virtualenv'
     ],
 )
