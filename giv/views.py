548a48f2ce87ee570ec149be1dd9e4204f711350
AndresSp <atunon@hypernovalabs.com>
2018-07-12 22:50:06 -0500

    Include Upload Functionality

--- giv/views.py
+++ giv/views.py
@@ -1,5 +1,6 @@
 import os
-from flask import send_from_directory, render_template
+from flask import send_from_directory, render_template, request
+from werkzeug import secure_filename
 from giv import app
 
 @app.route('/favicon.ico')
@@ -10,3 +11,14 @@ def favicon():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/upload', methods = ['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['file']
+        f.save(secure_filename(f.filename))
+        return 'Upload successfully'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
