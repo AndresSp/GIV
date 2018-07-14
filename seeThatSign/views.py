3b20c7659800d7f1e439c372a735e064530e5bac
AndresSp <atunon@hypernovalabs.com>
2018-07-14 08:30:57 -0500

    Improve feedback

--- seeThatSign/views.py
+++ seeThatSign/views.py
@@ -1,8 +1,10 @@
 import os
-from flask import send_from_directory, render_template, request
-from werkzeug import secure_filename
+from flask import flash, send_from_directory, render_template, request
+from werkzeug import secure_filename, exceptions
+from werkzeug.exceptions import HTTPException
 from seeThatSign import app
 
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config['UPLOAD_FOLDER']='./uploads'
 
 @app.route('/favicon.ico')
@@ -16,11 +18,16 @@ def index():
 
 @app.route('/upload', methods = ['GET', 'POST'])
 def upload_file():
+    upload = None
     if request.method == 'POST':
-        f = request.files['file']
-        filename = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-        return 'Upload successfully'
+        try:
+            f = request.files['file']
+            filename = secure_filename(f.filename)
+            f.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            upload = 'Upload Succesfully'
+        except HTTPException:
+            upload = 'Upload a file'
+    return render_template('index.html',upload=upload)
 
 def allowed_file(filename):
     return '.' in filename and \
